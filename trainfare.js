e5930670096971bb8b73f77166c047130755db44
CN 10 <administrator@CN-10.lan>
2019-07-10 17:28:09 +0100

    marks changes

--- trainfare.js
+++ trainfare.js
@@ -1,7 +1,6 @@
 const request = require('request')
 
-const station_code_from = "EUS"
-const station_code_to = "CTR"
+
 
 const trainFare = (station_code_from, station_code_to, callback) => {
     const url = `http://api.brfares.com/querysimple?orig=${station_code_from}&dest=${station_code_to}`
@@ -13,14 +12,14 @@ const trainFare = (station_code_from, station_code_to, callback) => {
             console.log(`hello 2 ${response.body.fares}`)
             callback("Unable to find train station location, try again", undefined)
         } else {
-            callback( undefined, response.body.fares[0].adult.fare
+            callback( undefined, response.body.fares[0]
                 
         );
         }
     })
 }
 
-trainFare('CTR','EUS', (error, response) => {
+trainFare('CTR','LIV', (error, response) => {
 if (error) {
     console.log(error)
 }
