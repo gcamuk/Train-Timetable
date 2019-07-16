856122ef8f3fb84d69d709bdfa16a974dad8934e
Gary <cam.dev2019@gmail.com>
2019-07-16 14:40:50 +0100

    creating table for results

--- old_files/app.js.sav.js
+++ old_files/app.js.sav.js
@@ -5,7 +5,7 @@ const express = require('express')
 const path = require('path')
 const port = process.env.PORT || 3006
 const app = express();
-const getFromTo = require('./getFromTo.js')
+const getFromTo = require('./getFromTo.js.js')
 const trainFare = require('./trainfare')
 
 //below hard coded for testing
